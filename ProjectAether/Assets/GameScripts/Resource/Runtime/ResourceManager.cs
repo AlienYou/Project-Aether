7e65373e4b19d0e7d4deaad96506beef0e11dbb7
Alien you <AlienYou1997@163.com>
2026-07-20 02:09:52 +0800

    [Resource][Fix] Remove extra code

--- ProjectAether/Assets/GameScripts/Resource/Runtime/ResourceManager.cs
+++ ProjectAether/Assets/GameScripts/Resource/Runtime/ResourceManager.cs
@@ -41,10 +41,6 @@ namespace ProjectAether.Resource
             var handle = await _provider.LoadAsync<T>(assetPath);
             if (handle.State == ResourceHandleState.Loaded)
             {
-                if (!ResourceCache.Add(key, handle))
-                {
-                    Log.Warning($"");
-                }
                 ResourceCache.Add(key, handle);
             }
             return handle;
